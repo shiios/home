520b56d589f9132b2ac6d82e067158d0dced0b9a
shiios <292826452@qq.com>
2017-10-30 15:01:39 +0800

    添加选择服务详情页面

---
+++ CommShare/Class/Service/GXServiceDetaiVC.h
@@ -0,0 +1,13 @@
+//
+//  GXServiceDetaiVC.h
+//  CommShare
+//
+//  Created by guang hui on 2017/10/30.
+//  Copyright © 2017年 Sandwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXServiceDetaiVC : GXBaseViewController
+
+@end
---
+++ CommShare/Class/Service/GXServiceDetaiVC.m
@@ -0,0 +1,40 @@
+//
+//  GXServiceDetaiVC.m
+//  CommShare
+//
+//  Created by guang hui on 2017/10/30.
+//  Copyright © 2017年 Sandwind. All rights reserved.
+//
+
+#import "GXServiceDetaiVC.h"
+
+@interface GXServiceDetaiVC ()
+
+@end
+
+@implementation GXServiceDetaiVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [GXGeneralHelp colorWithHexString:@"#ffffff" andAlpha:1.0];
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
